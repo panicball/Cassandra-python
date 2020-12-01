4772c1f1d2c16d81c9c9ee9e14b6702320d23a2b
Makstutyte <57039046+Makstutyte@users.noreply.github.com>
2020-12-01 14:20:27 +0200

    Update sandra.py

--- sandra.py
+++ sandra.py
@@ -23,23 +23,23 @@ session.execute('USE %s' % KEYSPACE)
 
 session.execute("""
     CREATE TABLE director (
-        id_d int,
+        id_director int,
         surname text,
         name text,
         imdb int,
-        PRIMARY KEY (id_d, imdb)
+        PRIMARY KEY (id_director, imdb)
 )
 """)
 
 
 session.execute("""
     CREATE TABLE film (
-        id_f int,
+        id_film int,
         title text,
         genre text,
         budget int,
-        id_d int,
-        PRIMARY KEY (id_d, id_f, budget)
+        iid_directord_d int,
+        PRIMARY KEY (id_director, id_film, budget)
 )
 """)
 
@@ -49,8 +49,8 @@ session.execute("""
         cinema_id int,
         time text,
         cost int,
-        id_f int,
-        PRIMARY KEY ((cinema_id, id_f), time)
+        id_film int,
+        PRIMARY KEY ((cinema_id, id_film), time)
 )
 """)
 
@@ -72,32 +72,32 @@ session.execute("INSERT INTO cinema (cinema_id, name, address, city, zipcode) VA
 session.execute("INSERT INTO cinema (cinema_id, name, address, city, zipcode) VALUES (113, 'Cinema 3', 'Address 3', 'City 3', '3333')")
 
 
-session.execute("INSERT INTO director (id_d, surname, name, imdb) VALUES (1, 'Bird', 'Brad', 100)")
-session.execute("INSERT INTO director (id_d, surname, name, imdb) VALUES (2, 'Adamson', 'Andrew', 101)") 
-session.execute("INSERT INTO director (id_d, surname, name, imdb) VALUES (3, 'Sharpsteen', 'Ben', 102)")  
-session.execute("INSERT INTO director (id_d, surname, name, imdb) VALUES (4, 'Unkrich', 'Lee', 103)") 
+session.execute("INSERT INTO director (id_director, surname, name, imdb) VALUES (1, 'Bird', 'Brad', 100)")
+session.execute("INSERT INTO director (id_director, surname, name, imdb) VALUES (2, 'Adamson', 'Andrew', 101)") 
+session.execute("INSERT INTO director (id_director, surname, name, imdb) VALUES (3, 'Sharpsteen', 'Ben', 102)")  
+session.execute("INSERT INTO director (id_director, surname, name, imdb) VALUES (4, 'Unkrich', 'Lee', 103)") 
 
 
-session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (1, 'Ratatouille', 'Fantasy', 150, 1)")
-session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (2, 'Shrek', 'Comedy', 125, 2)")  
-session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (3, 'Dumbo', 'Adventure', 13, 3)")  
-session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (4, 'The Incredibles', 'Adventure', 92, 1)")  
-session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (5, 'Coco', 'Adventure', 175, 4)") 
-session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (6, 'The Incredibles 2', 'Adventure', 200, 1)")   
-session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (7, 'Toy Story', 'Adventure', 30, 4)") 
+session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (1, 'Ratatouille', 'Fantasy', 150, 1)")
+session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (2, 'Shrek', 'Comedy', 125, 2)")  
+session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (3, 'Dumbo', 'Adventure', 13, 3)")  
+session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (4, 'The Incredibles', 'Adventure', 92, 1)")  
+session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (5, 'Coco', 'Adventure', 175, 4)") 
+session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (6, 'The Incredibles 2', 'Adventure', 200, 1)")   
+session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (7, 'Toy Story', 'Adventure', 30, 4)") 
 
 
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 111, '12:45', 10, 7)") 
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 112, '17:00', 12, 2)") 
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 111, '23:25', 15, 3)") 
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 113, '9:30', 6, 2)") 
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 112, '14:35', 6, 4)")
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 113, '21:17', 29, 1)")  
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 113, '7:00', 9, 5)") 
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 111, '20:50', 11, 6)") 
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 112, '9:30', 19, 7)")     
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 112, '19:30', 12, 7)")
-session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_f) VALUES ( 111, '19:30', 22, 7)")  
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 111, '12:45', 10, 7)") 
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 112, '17:00', 12, 2)") 
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 111, '23:25', 15, 3)") 
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 113, '9:30', 6, 2)") 
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 112, '14:35', 6, 4)")
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 113, '21:17', 29, 1)")  
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 113, '7:00', 9, 5)") 
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 111, '20:50', 11, 6)") 
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 112, '9:30', 19, 7)")     
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 112, '19:30', 12, 7)")
+session.execute("INSERT INTO movie_screening ( cinema_id, time, cost, id_film) VALUES ( 111, '19:30', 22, 7)")  
 
 
 def menu():
@@ -146,14 +146,14 @@ def director_and_all_films_querie():
     print(" ")
     print("Director: ")
 
-    directors = session.execute('SELECT name, surname FROM director WHERE id_d=1')
+    directors = session.execute('SELECT name, surname FROM director WHERE id_director=1')
     for director in directors:
         print("name:  ", director.name,"     surname:  ", director.surname)
 
     print(" ")
     print("Films: ")
 
-    rows = session.execute('SELECT title FROM film WHERE id_d=1')
+    rows = session.execute('SELECT title FROM film WHERE id_director=1')
     for row in rows:
         print("title:  ", row.title)
     print(" ")
@@ -163,7 +163,7 @@ def directors_imdb():
     print(" ")
     print("Directors: ")
 
-    rows = session.execute('SELECT name, surname, imdb FROM director WHERE id_d in (1, 2, 3, 4) AND imdb > 101')
+    rows = session.execute('SELECT name, surname, imdb FROM director WHERE id_director in (1, 2, 3, 4) AND imdb > 101')
     for row in rows:
         print("imdb:  ", row.imdb, "     name:    ", row.name,  "         surname:  ", row.surname)
     print(" ")
@@ -181,14 +181,14 @@ def all_director_and_all_films_querie():
     for i in range(count):
         print("------------------------------------------------------------")
         print("Director: ")
-        directors = session.execute('SELECT name, surname FROM director WHERE id_d = ' + str(i + 1))
+        directors = session.execute('SELECT name, surname FROM director WHERE id_director = ' + str(i + 1))
         for director in directors:
             print("name:  ", director.name,"     surname:  ", director.surname)
  
         print(" ")
         print("Films: ")
  
-        films = session.execute('SELECT title FROM film WHERE id_d = ' + str(i + 1))
+        films = session.execute('SELECT title FROM film WHERE id_director = ' + str(i + 1))
         for film in films:
             print("title:  ", film.title)
 
@@ -199,9 +199,9 @@ def film_in_cinema_querie():
     print(" ")
     print("Film: ")
 
-    rows = session.execute('SELECT id_f, title FROM film WHERE id_f=7 AND id_d=4')
+    rows = session.execute('SELECT id_film, title FROM film WHERE id_film=7 AND id_director=4')
     for row in rows:
-        print("id:    ", row.id_f, "     title:    ", row.title)  
+        print("id:    ", row.id_film, "     title:    ", row.title)  
 
     print(" ")
     print("Cinema: ")
@@ -213,9 +213,9 @@ def film_in_cinema_querie():
     print(" ")
     print("Movie screenings in specific cinema: ")
 
-    rows = session.execute("SELECT cinema_id, time, cost, id_f FROM movie_screening WHERE cinema_id=111 AND id_f=7 AND time in ('19:30','12:45') ")
+    rows = session.execute("SELECT cinema_id, time, cost, id_film FROM movie_screening WHERE cinema_id=111 AND id_film=7 AND time in ('19:30','12:45') ")
     for row in rows:
-        print("cinema id :  ", row.cinema_id, "     screening time:  ", row.time, "     cost:  ", row.cost, "     film id:  ", row.id_f)
+        print("cinema id :  ", row.cinema_id, "     screening time:  ", row.time, "     cost:  ", row.cost, "     film id:  ", row.id_film)
 
     print(" ")
 
@@ -232,16 +232,16 @@ def all_films_and_all_film_screenings_querie():
     for i in range(count):
         print("------------------------------------------------------------")
         print("Film: ")
-        films = session.execute('SELECT title FROM film WHERE id_d in (1, 2, 3, 4) AND id_f = ' + str(i + 1))
+        films = session.execute('SELECT title FROM film WHERE id_director in (1, 2, 3, 4) AND id_film = ' + str(i + 1))
         for film in films:
             print("title:  ", film.title)
  
         print(" ")
         print("Film screenings: ")
  
-        rows = session.execute('SELECT cinema_id, time, cost, id_f FROM movie_screening WHERE cinema_id in (111, 112, 113) AND id_f = ' + str(i + 1))
+        rows = session.execute('SELECT cinema_id, time, cost, id_film FROM movie_screening WHERE cinema_id in (111, 112, 113) AND id_film = ' + str(i + 1))
         for row in rows:
-            print("cinema id :  ", row.cinema_id, "     screening time:  ", row.time, "     cost:  ", row.cost, "     film id:  ", row.id_f)
+            print("cinema id :  ", row.cinema_id, "     screening time:  ", row.time, "     cost:  ", row.cost, "     film id:  ", row.id_film)
 
         print(" ")
 
@@ -265,9 +265,9 @@ def all_cinemas_and_all_film_screenings_querie():
         print(" ")
         print("Film screenings: ")
  
-        rows = session.execute('SELECT cinema_id, time, cost, id_f FROM movie_screening WHERE id_f in (1, 2, 3, 4, 5, 6, 7) AND cinema_id = ' + str(i + 111))
+        rows = session.execute('SELECT cinema_id, time, cost, id_film FROM movie_screening WHERE id_film in (1, 2, 3, 4, 5, 6, 7) AND cinema_id = ' + str(i + 111))
         for row in rows:
-            print("cinema id :  ", row.cinema_id, "     screening time:  ", row.time, "     cost:  ", row.cost, "     film id:  ", row.id_f)
+            print("cinema id :  ", row.cinema_id, "     screening time:  ", row.time, "     cost:  ", row.cost, "     film id:  ", row.id_film)
 
         print(" ")
 
@@ -283,10 +283,10 @@ def data_insertion_querie():
     print(" ")
     print("Inserting new films ")
 
-    session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (8, 'Toy Story 2', 'Adventure', 230, 4)IF NOT EXISTS")
-    session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (9, 'Toy Story 3', 'Adventure', 325, 4)IF NOT EXISTS")
-    session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (10, 'Toy Story 4', 'Adventure', 520, 4)IF NOT EXISTS")
-    session.execute("INSERT INTO film (id_f, title, genre, budget, id_d) VALUES (7, 'Toy Story', 'Fantasy', 30, 4)IF NOT EXISTS") # toks jau yra
+    session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (8, 'Toy Story 2', 'Adventure', 230, 4)IF NOT EXISTS")
+    session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (9, 'Toy Story 3', 'Adventure', 325, 4)IF NOT EXISTS")
+    session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (10, 'Toy Story 4', 'Adventure', 520, 4)IF NOT EXISTS")
+    session.execute("INSERT INTO film (id_film, title, genre, budget, id_director) VALUES (7, 'Toy Story', 'Fantasy', 30, 4)IF NOT EXISTS") # toks jau yra
     
 
 
@@ -302,7 +302,7 @@ def films_by_budget():
     print(" ")
     print("Films: ")
 
-    rows = session.execute('SELECT title, genre, budget FROM film WHERE id_d in (1, 2, 3, 4) AND id_f in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) AND budget > 150')
+    rows = session.execute('SELECT title, genre, budget FROM film WHERE id_director in (1, 2, 3, 4) AND id_film in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) AND budget > 150')
     for row in rows:
         print("title:  ", row.title, "\t\t\t genre:    ", row.genre,  "\t\t\t budget:  ", row.budget)
     print(" ")
